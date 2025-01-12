e41c243e751eb9616d6411f8fd49aa945a845b72
Ben Edgington <ben@benjaminion.xyz>
2025-01-12 20:35:47 +0000

    Limit precision to 6dp in income and cgt

--- src/bittytax/tax_event.py
+++ src/bittytax/tax_event.py
@@ -39,7 +39,7 @@ class TaxEventCapitalGains(TaxEvent):
             raise RuntimeError("Missing proceeds")
 
         self.disposal_type = disposal_type
-        self.quantity = s.quantity
+        self.quantity = s.quantity.quantize(Decimal("0.000000"))
         self.cost = cost.quantize(PRECISION)
         self.fees = fees.quantize(PRECISION)
         self.proceeds = s.proceeds.quantize(PRECISION)
@@ -70,7 +70,7 @@ class TaxEventIncome(TaxEvent):  # pylint: disable=too-few-public-methods
             raise RuntimeError("Missing cost")
 
         self.type = b.t_type
-        self.quantity = b.quantity
+        self.quantity = b.quantity.quantize(Decimal("0.000000"))
         self.amount = b.cost.quantize(PRECISION)
         self.note = b.wallet
         if b.fee_value:
